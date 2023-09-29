28dcf252fd3836c42257356ab5e48f5cee2465d0
marcusholloway17 <loganjean17@gmail.com>
2023-09-29 11:25:53 +0000

    add clrTopLevelAlert component

--- ngx-partials.module.ts
+++ ngx-partials.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestPipe } from './pipes/request.pipe';
 import { WithLoadingPipe } from './pipes/with-loading.pipe';
+import { ClrTopLevelAlertComponent } from './components/alerts/clr-top-level-alert/clr-top-level-alert.component';
 
 
 
 @NgModule({
   declarations: [
     RequestPipe,
-    WithLoadingPipe
+    WithLoadingPipe,
+    ClrTopLevelAlertComponent
   ],
   imports: [
     CommonModule
