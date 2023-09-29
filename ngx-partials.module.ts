8d88fd430831059c45466d6101d644300c2869e7
marcusholloway17 <loganjean17@gmail.com>
2023-09-29 14:26:45 +0000

    export ngx partials components, pipes, services ..

--- ngx-partials.module.ts
+++ ngx-partials.module.ts
@@ -1,14 +1,12 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RequestPipe } from './pipes/request.pipe';
-import { WithLoadingPipe } from './pipes/with-loading.pipe';
-import { ClrTopLevelAlertComponent } from './components/alerts/clr-top-level-alert/clr-top-level-alert.component';
-import { ClarityModule } from '@clr/angular';
-import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
-import { CardsComponent } from './components/cards/cards.component';
-import { RouterModule } from '@angular/router';
-
-
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RequestPipe } from "./pipes/request.pipe";
+import { WithLoadingPipe } from "./pipes/with-loading.pipe";
+import { ClrTopLevelAlertComponent } from "./components/alerts/clr-top-level-alert/clr-top-level-alert.component";
+import { ClarityModule } from "@clr/angular";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { CardsComponent } from "./components/cards/cards.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -16,12 +14,15 @@ import { RouterModule } from '@angular/router';
     WithLoadingPipe,
     ClrTopLevelAlertComponent,
     PageNotFoundComponent,
-    CardsComponent
+    CardsComponent,
   ],
-  imports: [
-    CommonModule,
-    ClarityModule,
-    RouterModule
-  ]
+  imports: [CommonModule, ClarityModule, RouterModule],
+  exports: [
+    RequestPipe,
+    WithLoadingPipe,
+    ClrTopLevelAlertComponent,
+    PageNotFoundComponent,
+    CardsComponent,
+  ],
 })
-export class NgxPartialsModule { }
+export class NgxPartialsModule {}
