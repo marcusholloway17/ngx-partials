13548caed0d7a9e091c328c04a6a82886a833430
marcusholloway17 <loganjean17@gmail.com>
2023-10-04 14:25:04 +0000

    create loader-five

---
+++ modules/ui-state/loaders/loader-five/loader-five.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-five',
+  templateUrl: './loader-five.component.html',
+  styleUrls: ['./loader-five.component.css']
+})
+export class LoaderFiveComponent {
+
+}
