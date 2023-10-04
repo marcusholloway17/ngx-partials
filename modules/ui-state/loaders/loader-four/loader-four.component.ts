7215cc0cb5569c9cb13eda92fafd3368b7fe1c6f
marcusholloway17 <loganjean17@gmail.com>
2023-10-04 14:24:21 +0000

    create loader-four

---
+++ modules/ui-state/loaders/loader-four/loader-four.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-four',
+  templateUrl: './loader-four.component.html',
+  styleUrls: ['./loader-four.component.css']
+})
+export class LoaderFourComponent {
+
+}
