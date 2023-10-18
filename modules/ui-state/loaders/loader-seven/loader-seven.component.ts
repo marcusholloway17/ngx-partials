25ad8e2888500ec7c01d71d08905f095c596d36a
marcusholloway17 <loganjean17@gmail.com>
2023-10-18 11:31:13 +0000

    create loader seven component

---
+++ modules/ui-state/loaders/loader-seven/loader-seven.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loader-seven',
+  templateUrl: './loader-seven.component.html',
+  styleUrls: ['./loader-seven.component.css']
+})
+export class LoaderSevenComponent {
+
+}
